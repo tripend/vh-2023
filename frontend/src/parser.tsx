c08172286e5aac7dc72ded354c125e2500e7585f
CornCube <mmalla2002@gmail.com>
2023-10-28 23:35:02 -0500

    fixed sorting

--- frontend/src/parser.tsx
+++ frontend/src/parser.tsx
@@ -14,7 +14,6 @@ interface UniversityData {
 }
 
 interface UniversityCardProps {
-  key: string;
   index: number;
   data: UniversityData;
 }
@@ -23,42 +22,49 @@ const convertOrgName = (orgName: string) => {
   return orgName.toLowerCase().replace(/\b[a-z]/g, (letter) => letter.toUpperCase());
 };
 
-const UniversityCard: React.FC<UniversityCardProps> = ({ key, index, data }) => (
-    <Card title={convertOrgName(data.ORGANIZATION)} style={{ width: '100%', marginBottom: '20px' }}>
-      <Row align="middle" gutter={16}>
-        <Col span={2}>
-          <p>{index + 1}</p>
-        </Col>
-        <Row>
-          <Divider type="vertical" style={{ marginLeft: '-30px', height: '40px'}} />
-        </Row>
-        <Col span={4}>
-          <p>State: {data.STATE}</p>
-          <p>City: {convertOrgName(data.CITY)}</p>
-        </Col>
-        <Col span={4}>
-          <p>Funding: {data.FUNDING}</p>
-        </Col>
-        <Col span={4}>
-          <p>Awards: {data.AWARDS}</p>
-        </Col>
-        <Col span={5}>
-        </Col>
-        <Col span={4}>
-          <ListRatingComponent category='Rating' funding={data.FUNDING} />
-        </Col>
+const UniversityCard: React.FC<UniversityCardProps> = ({ index, data }) => (
+  <Card title={convertOrgName(data.ORGANIZATION)} style={{ width: '100%', marginBottom: '20px' }}>
+    <Row align="middle" gutter={16}>
+      <Col span={2}>
+        <p>{index + 1}</p>
+      </Col>
+      <Row>
+        <Divider type="vertical" style={{ marginLeft: '-30px', height: '40px'}} />
       </Row>
-    </Card>
-  );
+      <Col span={4}>
+        <p>State: {data.STATE}</p>
+        <p>City: {convertOrgName(data.CITY)}</p>
+      </Col>
+      <Col span={4}>
+        <p>Funding: {data.FUNDING}</p>
+      </Col>
+      <Col span={4}>
+        <p>Awards: {data.AWARDS}</p>
+      </Col>
+      <Col span={5}>
+      </Col>
+      <Col span={4}>
+        <ListRatingComponent category='Rating' funding={data.FUNDING} />
+      </Col>
+    </Row>
+  </Card>
+);
 
-  export const processUniversities = (jsonData: UniversityData[]) => {
-    // Randomly pick 10 universities from the data
-    const shuffled = jsonData.sort(() => 0.5 - Math.random());
-    let selected = shuffled.slice(0, 10);
-    
-    // Sort them by their funding in descending order
-    selected = selected.sort((a, b) => parseFloat(b.FUNDING.replace(/,/g, '')) - parseFloat(a.FUNDING.replace(/,/g, '')));
+export const processUniversities = (jsonData: UniversityData[]) => {
+  // Randomly pick 10 universities from the data
+  const shuffled = jsonData.sort(() => 0.5 - Math.random());
+  let selected = shuffled.slice(0, 10);
   
-    // Return them as an array of UniversityCard components
-    return selected.map((university, index) => <UniversityCard key={university.ORGANIZATION} data={university} index={index} />);
-  };
+  // Sort them by their funding in descending order
+  selected = selected.sort((a, b) => {
+    const fundingA = parseFloat(a.FUNDING.replace(/[^0-9.]/g, ''));
+    const fundingB = parseFloat(b.FUNDING.replace(/[^0-9.]/g, ''));
+    return fundingB - fundingA;
+  });
+
+  // Return them as an array of UniversityCard components
+  return selected.map((university, index) => (
+    <UniversityCard key={university.ORGANIZATION} data={university} index={index} />
+  ));
+};
+  
